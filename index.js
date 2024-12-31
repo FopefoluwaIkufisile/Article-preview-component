2b7506068841dc858f5d52f2cbaa793a8c4ffcb5
FopefoluwaIkufisile <fope.ikufisile@gmail.com>
2024-12-31 01:33:59 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const shareBtn = document.querySelector(".share-btn")
+const sharePopUp = document.querySelector(".share-box2")
+const author = document.querySelector(".author-box");
+
+    shareBtn.addEventListener("click", ()=>{
+        const viewportWidth = window.innerWidth;
+
+        if (viewportWidth <= 700) {
+            sharePopUp.classList.toggle('active-small');
+            
+            console.log(`Clicked on small screen`);
+        } else {
+            sharePopUp.classList.toggle('active');
+            console.log(`Clicked on large screen`);
+        }
+      })
+
+
+
+
